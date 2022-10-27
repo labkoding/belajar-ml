4684f99b69fadec517fc5d170a7fb56501e0f864
Agung <kokeagung816@gmail.com>
2022-10-27 18:46:52 +0800

    commit

--- agung.py
+++ agung.py
@@ -1,9 +1,16 @@
-from flask import Flask
-from flask import jsonify
+from operator import index
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+df = pd.read_csv("data.csv")
 
-@app.route("/agung/hello")
-def api_hello_world():
-    message= "hello"
-    return jsonify ({'message': message
+df = df.rename(columns={"Engine HP": "HP", "Engine Cylinders": "Cylinders", "Transmission Type": "Transmission", "Driven_Wheels": "Drive Mode","highway MPG": "MPG-H", "city mpg": "MPG-C", "MSRP": "Price" })
+
+df = df.drop_duplicates()
+print(df.head(5))
+
+print(sns.boxplot(x=df['Price']))
+
+df.to_csv('agung.csv', index=False)
