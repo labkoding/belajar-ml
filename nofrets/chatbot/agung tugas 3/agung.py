e80846783660e555380886871768670d62f180ca
Agung <kokeagung816@gmail.com>
2022-11-22 21:36:56 +0800

    sipman tugas-6

--- nofrets/chatbot/agung tugas 3/agung.py
+++ nofrets/chatbot/agung tugas 3/agung.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import jsonify
 
 app = Flask(__name__)
-@app.route('/jissica/index')
+@app.route('/agung/index')
 def index():
     message = "hello world"
     return jsonify({'message': message})
