503cdb8d19baed1b8dbc8108b605157ac8bc7844
Agung <kokeagung816@gmail.com>
2022-11-23 18:02:26 +0800

    simpan

--- agung.py
+++ agung.py
@@ -1,9 +1,25 @@
+from unicodedata import name
+from xml.parsers.expat import model
 from flask import Flask
 from flask import jsonify
+from keras.models import load_model
+from PTL import Image, ImageOps 
+import numpy as np
+
 
 app = Flask(__name__)
-
-@app.route("/agung/hello")
+@app.route("/agung/upload-gambar")
 def api_hello_world():
-    message= "hello"
-    return jsonify ({'message': message
+    first_name= "agung"
+    last_name= "kurniawan"
+    age = 22
+    return jsonify ({
+        'first_name' : first_name,
+        'last_name' : last_name,
+          })
+
+@app.route('agung/absensi')
+def api_user():
+
+model = load_model('keras_model.h5')
+
