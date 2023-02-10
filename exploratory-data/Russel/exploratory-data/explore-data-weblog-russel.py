b344147eba13dccd991ec08bbfbd6e09151f924d
RusselPangkey <russelpangkey77@gmail.com>
2023-02-10 14:37:15 +0800

    Tugas 6 desember 2022 fix

--- exploratory-data/Russel/exploratory-data/explore-data-weblog-russel.py
+++ exploratory-data/Russel/exploratory-data/explore-data-weblog-russel.py
@@ -2,28 +2,35 @@ daftarIpArray = []
 countLogin = 0
 countLogout = 0
 loginUrlPerIP = {}
+logoutUrlPerIP = {}
 with open("C:/Users/Asus/belajar-ml/exploratory-data/nofrets/weblog.csv", "r") as f:
     for line in f:
-        # count string in line
-        if '/login.php' in line:
-            countLogin += 1
-        hasilSplit = line.split(',')
-        if '/logout.php' in line:
-            countLogout += 1
         hasilSplit = line.split(',')
         # check if string is ip address
         if len(hasilSplit[0].split('.')) == 4:
             # append if not exist
             if hasilSplit[0] not in daftarIpArray:
                 daftarIpArray.append(hasilSplit[0])
-    
+            
+            # count the number of login and logout accesses
+            if '/login.php' in line:
+                countLogin += 1
+                if hasilSplit[0] in loginUrlPerIP:
+                    loginUrlPerIP[hasilSplit[0]] += 1
+                else:
+                    loginUrlPerIP[hasilSplit[0]] = 1
+            if '/logout.php' in line:
+                countLogout += 1
+                if hasilSplit[0] in logoutUrlPerIP:
+                    logoutUrlPerIP[hasilSplit[0]] += 1
+                else:
+                    logoutUrlPerIP[hasilSplit[0]] = 1
 
-# contoh hasil akhir untuk variable loginUrlPerIP
-        if'10.128.2.1' in line:
-            loginUrlPerIP = line.count('10.128.2.1')
-loginUrlPerIP = {"10.128.2.1": loginUrlPerIP}
+# sort the daftarIpArray based on the number of login.php and logout.php accesses
+daftarIpArray.sort(key=lambda x: (loginUrlPerIP.get(x, 0), logoutUrlPerIP.get(x, 0)))
 
 print(daftarIpArray)
 print(countLogin)
 print(countLogout)
 print(loginUrlPerIP)
+print(logoutUrlPerIP)
