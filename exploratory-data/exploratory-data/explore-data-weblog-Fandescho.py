92af17e32df86ff369c7e49b4c056adb615ed3cf
whyfcm@gmail.com <whyfcm@gmail.com>
2023-02-10 14:55:34 +0800

    Fandescho Tugas 6 Desember

--- exploratory-data/exploratory-data/explore-data-weblog-Fandescho.py
+++ exploratory-data/exploratory-data/explore-data-weblog-Fandescho.py
@@ -2,12 +2,18 @@ daftarIpArray = []
 countLogin = 0
 countLogout = 0
 loginUrlPerIP = {}
-with open("C:/Users/HP/belajar-ml/exploratory-data/nofrets/weblog.csv", "r") as f:
+
+with open(r"C:\Users\HP\Downloads\belajar-ml\exploratory-data\nofrets\weblog.csv") as f:
     for line in f:
         # count string in line
         if '/login.php' in line:
             countLogin += 1
-        hasilSplit = line.split(',')
+            hasilSplit = line.split(',')
+            if len(hasilSplit[0].split('.')) == 4:
+                if hasilSplit[0] in loginUrlPerIP:
+                    loginUrlPerIP[hasilSplit[0]] += 1
+                else:
+                    loginUrlPerIP[hasilSplit[0]] = 1
         if '/logout.php' in line:
             countLogout += 1
         hasilSplit = line.split(',')
@@ -16,14 +22,10 @@ with open("C:/Users/HP/belajar-ml/exploratory-data/nofrets/weblog.csv", "r") as 
             # append if not exist
             if hasilSplit[0] not in daftarIpArray:
                 daftarIpArray.append(hasilSplit[0])
-    
 
-# contoh hasil akhir untuk variable loginUrlPerIP
-        if'10.128.2.1' in line:
-            loginUrlPerIP = line.count('10.128.2.1')
-loginUrlPerIP = {"10.128.2.1": loginUrlPerIP}
+# sort daftarIpArray based on the number of login.php accesses
+sortedIpArray = sorted(loginUrlPerIP.items(), key=lambda x: x[1], reverse=True)
 
-print(daftarIpArray)
+print(sortedIpArray)
 print(countLogin)
 print(countLogout)
-print(loginUrlPerIP)
